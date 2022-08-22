5689ff69b1509ca04d0f2d808b50509d33f1904c
Pavel Popov <pcveykos@gmail.com>
2022-08-22 16:25:11 +0200

    Add some styling additions

--- scripts/index.js
+++ scripts/index.js
@@ -4,16 +4,9 @@ import { Board } from "./Board.js";
 import { AircraftCarrier, Ship, Battlecruiser } from "./Ship.js";
 import { Game } from "./Game.js"
 
-let acVert = document.querySelector('#AC1')
-let acHor = document.querySelector('#AC2')
-let bcVert = document.querySelector('#BC1')
-let bcHor = document.querySelector('#BC2')
-let sh1 = document.querySelector('#SH1')
-let sh2 = document.querySelector('#SH2')
-
-
 const game = new Game;
 game.start()
+
 
 console.log(game.player,game.enemy)
 console.log(game)
