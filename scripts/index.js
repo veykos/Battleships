c926c28c8e7f8e6d3813fb05f0c0652c957a3d9a
Pavel Popov <pcveykos@gmail.com>
2022-08-24 14:41:19 +0200

    Add welcome message to the player and reloading of the page on final message

--- scripts/index.js
+++ scripts/index.js
@@ -5,9 +5,22 @@ import { AircraftCarrier, Ship, Battlecruiser } from "./Ship.js";
 import { Game } from "./Game.js"
 import { Bot } from "./Bot.js";
 
+const welcomeDiv = document.querySelector('.welcome');
+const gameContainer = document.querySelector('.game-container-wrapper')
+const finalMessage = document.querySelector('.final-message')
+
+welcomeDiv.addEventListener('click', event => {
+    welcomeDiv.classList.add('hidden')
+    gameContainer.classList.remove('hidden')
+})
+finalMessage.addEventListener('click', event => {
+    location.reload()
+})
+
 
 const game = new Game;
 game.start()
+
 
 
 
